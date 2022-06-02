a2e3f1ed4bc71cba49cdf2115c2244dc3445ed6b
lujinzhong <602060558@qq.com>
2022-06-02 08:27:59 +0800

    临时提交

---
+++ leetcode21(合并两个链表)/main/main.go
@@ -0,0 +1,17 @@
+package main
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	return nil
+}
+
+func main() {
+
+}
