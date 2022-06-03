cd1241a0ab8f99adec249fb9d450bfa975678e2f
lujz01 <lujz01@mingyuanyun.com>
2022-06-03 16:51:21 +0800

    feat(21)合并两个链表-递归法

--- leetcode21(合并两个链表)/main/main.go
+++ leetcode21(合并两个链表)/main/main.go
@@ -55,6 +55,21 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	return list3
 }
 
+// 递归法
+func mergeTwoListsV2(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}else if list2 == nil {
+		return list1
+	}else if list1.Val <= list2.Val {
+		list1.Next = mergeTwoListsV2(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoListsV2(list1, list2.Next)
+		return list2
+	}
+}
+
 func main() {
 
 }
