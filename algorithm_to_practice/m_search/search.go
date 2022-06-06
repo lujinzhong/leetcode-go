03bafcec3e70c85c27cb55c683048905007c1a81
lujz01 <lujz01@mingyuanyun.com>
2022-06-06 23:16:15 +0800

    feat(算法练习)二分法-递归版本

--- algorithm_to_practice/m_search/search.go
+++ algorithm_to_practice/m_search/search.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 // 标准二分法
-func binarySearch(arr [8]int, val int) int {
+func binarySearch(arr []int, val int) int {
 	low := 0
 	high := len(arr) - 1
 	for low <= high {
-		mid := (high + low) / 2
+		mid := (high + low) / 2 // low+(high-low)/2 这种可以避免溢出
 		if arr[mid] == val {
 			return mid
 		} else if arr[mid] > val {
@@ -19,8 +19,23 @@ func binarySearch(arr [8]int, val int) int {
 	return -1
 }
 
-func main() {
-	arr := [8]int{1,2,3,4,5,6,7,8}
-	fmt.Println(binarySearch(arr, 8))
+// 递归版本
+func binarySearchV2(arr []int, val int) int {
+	return binaryRecursiveSearch(arr, 0, len(arr) -1, val)
+}
 
+func binaryRecursiveSearch(arr[]int, low, high, val int) int{
+	mid := low + (high-low)/2
+	if arr[mid] == val {
+		return mid
+	}else if arr[mid] > val {
+		return binaryRecursiveSearch(arr, low, mid - 1, val)
+	}else {
+		return binaryRecursiveSearch(arr, mid + 1, high, val)
+	}
+}
+
+func main() {
+	fmt.Println(binarySearch([]int{1,2,3,4,5,6,7,8}, 8))
+	fmt.Println(binarySearchV2([]int{1,3,5,10,12}, 10))
 }
