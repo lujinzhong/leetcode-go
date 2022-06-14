541a82ced94fad18050cd90eeb7cb9e104154fb7
lujz01 <lujz01@mingyuanyun.com>
2022-06-14 22:13:48 +0800

    feat(剑指offer58)

---
+++ 剑指offer58( II. 左旋转字符串)/main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+//字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+
+//示例 1：
+//
+//输入: s = "abcdefg", k = 2
+//输出: "cdefgab"
+//示例 2：
+//
+//输入: s = "lrloseumgh", k = 6
+//输出: "umghlrlose"
+
+// 暴力解法
+func reverseLeftWords(s string, n int) string {
+	var newStr, subStr string
+	for k, v := range s {
+		if k < n {
+			subStr = subStr + string(v)
+		} else {
+			newStr = newStr + string(v)
+		}
+	}
+	return newStr + subStr
+}
+
+// 切片原理
+func reverseLeftWordsV2(s string, n int) string {
+	return s[n:] + s[:n]
+}
+func main() {
+	fmt.Println(reverseLeftWords("abcdeffff", 5))
+}
