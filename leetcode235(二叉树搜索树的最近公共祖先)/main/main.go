5450b71b6399e680c888973e520467904556ecb0
lujz01 <lujz01@mingyuanyun.com>
2022-08-01 21:46:46 +0800

    feat(235)二叉树搜索树的最近公共祖先

---
+++ leetcode235(二叉树搜索树的最近公共祖先)/main/main.go
@@ -0,0 +1,34 @@
+package main
+
+//给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+//
+//百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+//
+//例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+//
+//所有节点的值都是唯一的。
+//p、q 为不同节点且均存在于给定的二叉搜索树中。
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		return root
+	}
+
+}
+func main() {
+
+}
